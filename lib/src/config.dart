39e6a313d272a50f94012d80867ef492c4921231
guyluz11 <guyluz11@gmail.com>
2022-12-07 15:05:47 +0200

    Reverting back to python 3.8.10 to test some fix for cffi python pacakge fail to install inside snap

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -8,11 +8,11 @@ String appDir = path.join(
 String tempDir = path.join(appDir, "temp");
 String instanceDir = path.join(appDir, "instances");
 
-String defaultPythonVersion = "3.9.13";
+String defaultPythonVersion = "3.8.10";
 String defaultPythonPath = "python3";
 
 String checkPythonVersion(String rawPythonVersion) {
-  String realPythonVersion = "3.9.13";
+  String realPythonVersion = "3.8.10";
 
   var versions = rawPythonVersion.split(".");
   if (versions.length == 3) {
